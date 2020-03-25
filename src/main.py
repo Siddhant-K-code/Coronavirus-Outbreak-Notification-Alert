24857eccf057b6b4b044a879c572d6d39ac0aadc
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-03-25 11:44:48 +0530

    main.py

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def notifyMe(title, message):
     notification.notify(
         title = title,
         message = message,
-        app_icon = "C:\Coronavirus Outbreak Notification\Icon.ico",
+        app_icon = "C:\Coronavirus Outbreak Notification\Icon.ico", # app icon = "<path>",
         timeout = 3
     )
 
@@ -19,7 +19,7 @@ def getData(url):
 if __name__ == "__main__":
     while True:
         # notifyMe("Siddhant", " Let's Stop the spread of the virus together")
-        myHtmlData = getData('https://www.mohfw.gov.in/')
+        myHtmlData = getData('https://www.mohfw.gov.in/') # Official website of Ministry of Health and Family Welfare of Gov. of India
 
         
         soup = BeautifulSoup(myHtmlData, 'html.parser')
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         myDataStr = myDataStr[1:]     
         itemList = myDataStr.split("\n\n")    
         
-        states = ['Madhya Pradesh']
+        states = ['Madhya Pradesh'] # You can add more states , and the state which you want
         for item in itemList [0:25]:
             dataList = item.split('\n')
             if dataList[1] in states:
@@ -39,5 +39,5 @@ if __name__ == "__main__":
                 nText = f"State : {dataList[1]}\nIndian : {dataList[2]} & Foreign : {dataList[3]}\nCured : {dataList[4]}\nDeaths : {dataList[5]} "
                 notifyMe(nTitle, nText)
                 time.sleep(2)
-        time.sleep(3600)
+        time.sleep(3600)  # it will give alert in regular period , i take it as 3600 Seconds , i.e. 1 Hour 
 
